4213ee273bd65b260f96b397e82895c4a65cca55
Kolge04 <117319718+Kolge04@users.noreply.github.com>
2022-11-07 13:11:32 +0400

    Update reklam.py

--- handlers/reklam.py
+++ handlers/reklam.py
@@ -14,17 +14,17 @@ async def broadcast(_, message: Message):
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Reklam yayını başlıyor...`")
+        wtf = await message.reply("⚡ Reklam Göndərilməyə Başlayır ...")
         if not message.reply_to_message:
-            await wtf.edit("**__Lütfen mesajı bekleyiniz...__**")
+            await wtf.edit("**__Zəhmət Olmasa Mesajı Gözləyin...__**")
             return
         lmao = message.reply_to_message.text
         async for dialog in aditya.iter_dialogs():
             try:
                 await aditya.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Reklam başarıyla iletildi` \n\n**Gönderildiği yer:** `{sent}` Sohbet \n**Başarısız oldu:** {failed} Sohbet")
+                await wtf.edit(f"✅ Reklam Uğurla Göndərildi\n\n**📑 Gönderildiği yer:** `{sent}` Söhbət \n**❌ Uğursuz Oldu:** {failed} Söhbət")
                 await asyncio.sleep(3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`Reklam başarıyla iletildi' \n\n**gönderildiği yer:** `{sent}` Sohbet \n**başarısız oldu:** {failed} Sohbet")
+        await message.reply_text(f"✅ Reklam Uğurla Göndərildi \n\n**📑 Göndərildiyi Yer:** `{sent}` Söhbət \n**❌ Uğursuz Oldu:** {failed} Söhbət")
