5b5ab638ec46d737224a5731e0c463025afc02f5
Kolge04 <117319718+Kolge04@users.noreply.github.com>
2022-11-07 10:09:06 +0400

    Update botjoin.py

--- handlers/botjoin.py
+++ handlers/botjoin.py
@@ -6,7 +6,7 @@ from helpers.decorators import errors, authorized_users_only
 from config import SUDO_USERS 
 import asyncio
 
-@Client.on_message(filters.group & filters.command(["katil", "asistan"]))
+@Client.on_message(filters.group & filters.command(["qatıl", "asistan"]))
 @authorized_users_only
 @errors
 async def addchannel(client, message):
@@ -15,7 +15,7 @@ async def addchannel(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except:
         await message.reply_text(
-            "<b>Beni Önce Yönetici Yapmalısın</b>",
+            "<b>ℹ Mənə İlk Olaraq Yetki Verməlisən\n\n✅ Mesaj Silmə\n✅ Bağlantı İlə Dəvət Etmə\n✅ Səsli Söhbəti Yönətmə</b>",
         )
         return
 
@@ -26,64 +26,64 @@ async def addchannel(client, message):
 
     try:
         await USER.join_chat(invitelink)
-        await USER.send_message(message.chat.id,"Senin İsteğin Üzerine Geldim")
+        await USER.send_message(message.chat.id,"😀 Sen İsdəyin Üçün Gəldim")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>Asistan zaten bu grupta 🙄</b>",
+            "<b>✅ Asistan Onsuzda Bu Qrupda Var</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
-            f"<b>🔵 Zaman Aşımı Hatası 🔵\n User {user.first_name} userbot için yoğun katılma istekleri nedeniyle grubunuza katılamadı! Asistanın grupta yasaklanmadığından emin olun."
-            "\n\n Yada Asistan Hesabını Gruba Kendin Ekle </b>",
+            f"<b>🔵 Zaman Aşımı Xətası 🔵\n User {user.first_name} Userbot Üçün Böyük Qatılma İsdəkləri Üçün Qrupa Qatılamadı! Asistanın qrupda Əngənlənmədiyindən əmin olun."
+            "\n\n Yada Asistan Hesabını Qruba Özün Ekle </b>",
         )
         return
     await message.reply_text(
-            "<b>Asistan zaten bu grupta 🙄</b>",
+            "<b>✅ Asistan Onsuzda Bu Qrupda Var</b>",
         )
     
-@USER.on_message(filters.group & filters.command(["ayril", "asistanby"]))
+@USER.on_message(filters.group & filters.command(["ayrıl", "asistanat"]))
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
     except:  
         await message.reply_text(
-            f"<b>Kullanıcı grubunuzdan ayrılamadı!."
-            "\n\nYada Kendin Çıkarabilirsin</b>",
+            f"<b>❌ Asistan Qrubunuzdan Ayrılamadı!."
+            "\n\n✅ Yada Özün Çıxarda Bilərsən</b>",
         )
         return
  
-@USER.on_message(filters.group & filters.command(["userbotleave"]))
+@USER.on_message(filters.group & filters.command(["userbotat", "userbotleave"]))
 @authorized_users_only
 async def rem(USER, message):
     try:
         await USER.leave_chat(message.chat.id)
     except:
         await message.reply_text(
-            f"<b>Kullanıcı grubunuzdan ayrılamadı! Sel bekliyor olabilir."
-            "\n\nVeya beni Grubunuzdan manuel olarak tekmeleyin</b>",
+            f"<b>❌ Userbot Qrubunuzdan Ayrılamadı!."
+            "\n\n✅ Vəya Məni Qrubunuzdan manuel Olaraq Atin</b>",
         )
         return
 
 
-@Client.on_message(filters.command(["maho"]))
+@Client.on_message(filters.command(["tamayrıl", "maho"]))
 async def bye(client, message):
     if message.from_user.id in SUDO_USERS:
         left = 0
         failed = 0
-        lol = await message.reply("Tüm Gruplardan ayrılıyorum...")
+        lol = await message.reply("✅ Bütün Qruplardan Ayrılıram...")
         async for dialog in USER.iter_dialogs():
             try:
                 await USER.leave_chat(dialog.chat.id)
                 left = left + 1
                 await lol.edit(
-                    f"Ayrılıyorum... Left: {left} chats. Failed: {failed} chats."
+                    f"Ayrılıram... Left: {left} chats. Failed: {failed} chats."
                 )
             except:
                 failed = failed + 1
                 await lol.edit(
-                    f"Ayrılıyorum... Left: {left} chats. Failed: {failed} chats."
+                    f"Ayrılıram... Left: {left} chats. Failed: {failed} chats."
                 )
             await asyncio.sleep(0.7)
         await client.send_message(
